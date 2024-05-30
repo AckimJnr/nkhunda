845c79ab8d7783ab799611550c240daa1f217bcc
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-05-30 14:24:30 +0200

    Add messaging job

---
+++ api/v1/jobs/messaging_job.py
@@ -0,0 +1,17 @@
+from models.message import Message
+from rq import Queue
+from redis import Redis
+
+
+def send_message_job(message: Message):
+    """
+    Send message job
+    """
+    try:
+        redis_conn = Redis()
+        q = Queue("message", connection=redis_conn)
+        job_id = f"{message.app_id}-{message.recipient_id}"
+        q.enqueue("send_message", dict(message), job_id=job_id)
+        return True
+    except Exception as e:
+        return False
